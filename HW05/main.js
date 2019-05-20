3b91eb3982752226fffc1c695285579687b6cc65
omgazero <omgazero@gmail.com>
2019-05-21 02:16:11 +0800

    HW05

--- HW05/main.js
+++ HW05/main.js
@@ -3,8 +3,9 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 var players = [];
+var steps = [];
 var playerID = 0;
-
+var start = false;
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -12,20 +13,34 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   console.log('player ' + playerID + ' connected');
   socket.emit('ID', playerID);
-  players.push({id: playerID, step: false});
+
+  let hue = Math.random();
+  players.push({id: playerID, hue:hue});
+  steps.push({id: playerID, step: false});
 
   io.emit('update_status', players);
 
   ++playerID;
 
-  socket.on('step', function(msg){
-    console.log('player ' + msg.id + ' step = ' + msg.step);
+  if(playerID === 4)
+    if(start!=true){
+      start = !start;
+      console.log('game start!');
+      io.emit('game_start', start);
+    }
+
+  socket.on('step', function(data){
+    console.log('player ' + data.id + ' step = ' + data.step);
     let i;
     for (i = 0; i < players.length; i++) {
-  	  if (players[i].id === msg.id) break;
+  	  if (steps[i].id === data.id) break;
   	};
-    players[i].step = msg.step;
-    io.emit('update_status', players);
+    steps[i].step = data.step;
+    io.emit('update_step', steps);
+  });
+  ///sync
+  socket.on ('pos_now', function (data) {
+  	socket.broadcast.emit ('pos_now', data);
   });
 });
 
